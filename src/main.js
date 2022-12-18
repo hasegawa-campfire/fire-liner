4f5b1293e0933e5d2860da94f1c675394875923a
koichi.hasegawa <koichi.hasegawa@camp-fire.jp>
2022-12-19 00:11:53 +0900

    実装

--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
-import { g, screen } from '@/r.js'
+import { Tween } from './local_modules/tween.js'
+import { audioReady } from '@local_modules/audio.js'
+import { g, mainLoop, pointer, pref, screen, store } from '@/r.js'
+import { Dialog } from '@/ui/dialog.js'
+import { SceneTitle } from '@/scenes/title/index.js'
+import { SceneLevels } from '@/scenes/levels/index.js'
+import { ScenePlay } from '@/scenes/play/index.js'
+import { SceneRanking } from '@/scenes/ranking/index.js'
+import { SceneTips } from '@/scenes/tips/index.js'
+import { playAudioBgm, setAudioMute, stopAudioSe } from '@/helper.js'
+import { themeCount } from '@/data/theme.js'
 
 const offScreenShaders = {
   vertex: `#version 300 es
 in vec2 position;
 in vec3 textureCoord;
+in float luminance;
 out vec3 vTextureCoord;
+out float vLuminance;
 
 uniform vec2 resolution;
 uniform float flipY;
 
 void main() {
   vTextureCoord = textureCoord;
+  vLuminance = luminance;
   vec2 clipSpace = (position / resolution) * 2.0 - 1.0;
   gl_Position = vec4(clipSpace * vec2(1, flipY), 0, 1);
 }
@@ -20,12 +33,20 @@ precision mediump float;
 precision mediump sampler2DArray;
 
 uniform sampler2DArray textureArray;
+uniform sampler2D paletteTexture;
+uniform float palettePattern;
 
 in vec3 vTextureCoord;
+in float vLuminance;
 out vec4 outColor;
 
+const vec3 grayScale = vec3(0.299, 0.587, 0.114);
+
 void main() {
-  outColor = texture(textureArray, vTextureCoord);
+  vec4 textureColor = texture(textureArray, vTextureCoord);
+  float gray = clamp(dot(textureColor.rgb, grayScale) + vLuminance, 0.0, 1.0);
+  vec4 paletteColor = texture(paletteTexture, vec2(gray, palettePattern));
+  outColor = vec4(paletteColor.rgb, textureColor.a);
 }
 `,
 }
@@ -34,12 +55,14 @@ const onScreenShaders = {
   vertex: `#version 300 es
 in vec2 position;
 in vec3 textureCoord;
+out vec2 vPosition;
 out vec3 vTextureCoord;
 
 uniform vec2 resolution;
 uniform float flipY;
 
 void main() {
+  vPosition = position;
   vTextureCoord = textureCoord;
   vec2 clipSpace = (position / resolution) * 2.0 - 1.0;
   gl_Position = vec4(clipSpace * vec2(1, flipY), 0, 1);
@@ -51,11 +74,16 @@ precision mediump sampler2DArray;
 
 uniform sampler2DArray textureArray;
 
+in vec2 vPosition;
 in vec3 vTextureCoord;
 out vec4 outColor;
 
 void main() {
   outColor = texture(textureArray, vTextureCoord);
+  float x = mod(vPosition.x, 1.0);
+  float y = mod(vPosition.y, 1.0);
+  float edge = pow(max(x, y), 10.0);
+  outColor.rgb += (1.0 - outColor.rgb) * edge * 0.6;
 }
 `,
 }
@@ -65,11 +93,14 @@ const offScreenProgram = g.program.create({
   attrs: {
     position: 2,
     textureCoord: 3,
+    luminance: 1,
   },
   uniforms: {
     resolution: '2f',
     flipY: '1f',
     textureArray: '1i',
+    paletteTexture: '1i',
+    palettePattern: '1f',
   },
 })
 
@@ -88,18 +119,82 @@ const onScreenProgram = g.program.create({
 
 const offScreen = g.offScreen.create(screen.width, screen.height)
 
-function main() {
-  requestAnimationFrame(main)
+const fadeOut = Tween.from(0).to(4, 16)
+const fadeIn = Tween.from(4).to(0, 16).start()
 
+setAudioMute(pref.mute)
+audioReady.then(stopAudioSe)
+
+/**
+ * @typedef Scene
+ * @prop {() => void} update
+ * @prop {import('@local_modules/audio.js').Audio } bgm
+ */
+
+/** @type {Record<string,new (...args: any[]) => Scene>} */
+const sceneNames = {
+  title: SceneTitle,
+  ranking: SceneRanking,
+  levels: SceneLevels,
+  play: ScenePlay,
+  tips: SceneTips,
+}
+
+/** @type {Scene} */
+let scene = new SceneTitle()
+
+function main() {
   offScreen.begin()
   offScreenProgram.begin()
   offScreenProgram.setUniforms({
     resolution: [offScreen.width, offScreen.height],
     flipY: [1],
     textureArray: [0],
+    paletteTexture: [2],
+    palettePattern: [(pref.theme + 0.5) / themeCount],
   })
 
-  g.drawRect(0, 0, screen.width, screen.height, '#000')
+  fadeOut.next()
+  fadeIn.next()
+
+  if (store.nextScene) {
+    if (!fadeOut.started) {
+      fadeOut.start()
+    }
+    if (fadeOut.onDone) {
+      const [name, ...args] = store.nextScene
+      store.nextScene = null
+      const prevScene = scene
+      scene = new sceneNames[name](...args)
+      fadeOut.reset()
+      fadeIn.start()
+      if (prevScene.bgm !== scene.bgm) {
+        prevScene.bgm.setVolume(0, 0.3)
+      }
+    }
+  }
+
+  if (fadeOut.running) {
+    g.setAttr('luminance', [Math.ceil(fadeOut.value) / 3])
+    pointer.disabled = true
+  } else if (fadeIn.running) {
+    g.setAttr('luminance', [Math.ceil(fadeIn.value) / 3])
+    pointer.disabled = false
+  } else {
+    g.setAttr('luminance', [0])
+    pointer.disabled = false
+  }
+
+  if (fadeIn.onDone) {
+    playAudioBgm(scene.bgm)
+  }
+
+  pointer.update()
+  Dialog.updateBegin()
+
+  g.save()
+  scene.update()
+  g.restore()
 
   offScreenProgram.end()
   offScreen.end()
@@ -114,7 +209,11 @@ function main() {
   g.drawImage(offScreen)
   onScreenProgram.end()
   g.stdout.end()
+
+  Dialog.updateEnd()
 }
 
-requestAnimationFrame(main)
-screen.canvas.hidden = false
+g.loadTexture(2, 'colors.png').then(() => {
+  mainLoop.proc = main
+  screen.canvas.hidden = false
+})
--- src/r.js
+++ src/r.js
@@ -1,5 +1,24 @@
 import { Screen } from '@local_modules/screen.js'
 import { Graphics } from '@local_modules/graphics/index.js'
+import { BmpFont } from '@local_modules/font/index.js'
+import { InputPointer } from '@local_modules/input/index.js'
+import { createPref } from '@local_modules/pref.js'
+import { Random } from '@local_modules/random.js'
+import { DataLog } from '@local_modules/data-log/index.js'
+import { TweenManager } from '@local_modules/tween.js'
+import { Audio } from '@local_modules/audio.js'
+import { Loop } from '@local_modules/loop.js'
+
+export const tileSize = 24
+export const maxLevel = 15
+export const defaultName = 'NO NAME'
+
+export const color = {
+  black: '#000',
+  dark: '#555',
+  light: '#aaa',
+  white: '#fff',
+}
 
 export const screen = new Screen({
   el: '#screen',
@@ -13,3 +32,106 @@ export const g = new Graphics({
   texLayers: 10,
   clearColor: '#fff',
 })
+
+g.font = BmpFont.load('misaki_gothic.png')
+
+export const mainLoop = new Loop({ fps: 60 })
+export const pointer = new InputPointer(screen.canvas, screen)
+export const random = new Random()
+export const dataLog = new DataLog(2)
+export const tween = new TweenManager()
+
+export const store = {
+  /** @type {[String, ...unknown[]] | null} */
+  nextScene: null,
+  puzzle: false,
+  level: 0,
+  replay: false,
+  time: 0,
+  timeLimit: 0,
+  score: 0,
+  newRecord: false,
+  rate: 0,
+  routeCount: 0,
+
+  get timeIn() {
+    return 0 <= this.time && this.time < this.timeLimit
+  },
+
+  get timeOver() {
+    return this.time >= this.timeLimit
+  },
+}
+
+export const pref = createPref('fire-liner', {
+  levels: /** @type {{star: number, score: number}[]} */ ([]),
+  tips: /** @type {boolean[]} */ ([]),
+  theme: 0,
+  puzzleScore: /** @type {boolean | null} */ (null),
+  mute: false,
+})
+
+export const bgm = {
+  menu: Audio.load('audio/GB-Fighting-A02-2(Story-Loop115).mp3', {
+    loop: true,
+  }),
+  puzzle1: Audio.load('audio/GB-General-B02-2(Stage1-Loop100).mp3', {
+    loop: true,
+    when: 0.8,
+  }),
+  puzzle2: Audio.load('audio/GB-General-B04-2(Stage3-Loop150).mp3', {
+    loop: true,
+    when: 0.8,
+  }),
+  scoreTrial: Audio.load('audio/GB-Racing-A03-2(Stage1-Loop180).mp3', {
+    loop: true,
+    when: 0.8,
+  }),
+  clear: Audio.load('audio/GB-General-B07-1(Clear1).mp3'),
+  finish: Audio.load('audio/GB-Racing-A09-2(Clear2).mp3'),
+}
+
+export const se = {
+  buttonDown: Audio.load('audio/GB-General03-04(Noise)-a.mp3', {
+    minTime: 0,
+    volume: 0.4,
+  }),
+  buttonUp: Audio.load('audio/GB-General03-04(Noise)-b.mp3', {
+    minTime: 0,
+    volume: 0.2,
+  }),
+  lock: Audio.load('audio/GB-RPG01-5(Enter).mp3', {
+    volume: 0.67,
+  }),
+  dialogOpen: Audio.load('audio/GB-General01-08(Pitch).mp3', {
+    minTime: 0,
+    volume: 0.55,
+  }),
+  clear: Audio.load('audio/GB-General01-03(Pitch).mp3', {
+    volume: 0.33,
+    when: 0.5,
+  }),
+  allClear: Audio.load('audio/GB-RPG-B14-4(Victory-Short).mp3', {
+    volume: 0.9,
+  }),
+  finish: Audio.load('audio/GB-Shooter01-02(Shoot).mp3'),
+  move: Audio.load('audio/GB-Action01-03(Throw).mp3', {
+    volume: 0.5,
+  }),
+  notMove: Audio.load('audio/GB-Action01-04(Damage).mp3', {
+    volume: 0.6,
+  }),
+  reverse: Audio.load('audio/GB-Fighting01-05(Attack).mp3', {
+    volume: 0.3,
+  }),
+  switch: Audio.load('audio/GB-General03-04(Noise).mp3', {
+    volume: 0.5,
+  }),
+  spark: Audio.load('audio/GB-General02-02(Pitch).mp3', {
+    volume: 0.7,
+  }),
+  strokeAdd: Audio.load('audio/GB-General03-04(Noise)-c.mp3', {
+    volume: 0.33,
+    minTime: 0.02,
+  }),
+}
